6b3d50351a4c4abb352f688e0b2008fc87812857
Puneeth888-br <60321269+Puneeth888-br@users.noreply.github.com>
2024-02-28 12:51:26 +0530

    Add files via upload

---
+++ UI_Team/Puneeth/ChessUserDefined/main.h
@@ -0,0 +1,34 @@
+typedef enum
+{
+    STATUS_OK = 0,
+    STATUS_ERR
+}RET_STATUS;
+
+
+typedef enum
+{
+    BOARD_IN = 0,
+    BOARD_END
+}BOARD_STATUS;
+
+typedef enum
+{
+    VIRGIN = 0,
+    REPEAT
+}REPEATER;
+
+
+typedef enum
+{
+    LOSS = 0,
+    WON,
+    PLAY
+}GAMESTATUS;
+
+
+typedef enum
+{
+    DEAD = 0,
+    ALIVE,
+}MOVE_STATUS;
+
